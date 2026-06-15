3c41246b556751cb8ad67b52c43e5de1c882f1c0
madhuu1706 <prmadhumithan@gmail.com>
2026-06-15 11:36:16 +0530

    Delete vyse-deploy-ready/vyse directory

--- vyse-deploy-ready/vyse/apps/api/vyse_api/__init__.py
+++
@@ -1,2 +0,0 @@
-"""VYSE backend package. API and worker share this codebase (two entrypoints)."""
-__version__ = "0.1.0"
--- vyse-deploy-ready/vyse/apps/api/vyse_api/deps.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from .auth import Ctx, current_ctx
-from .db import get_session
-
-__all__ = ["Ctx", "current_ctx", "get_session", "AsyncSession", "Depends"]
